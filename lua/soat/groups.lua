64e156281c96c28459254a116593d05f731cc92e
Marian Guceanu <marian.guceanu@yahoo.com>
2026-06-12 22:23:33 +0300

    i have no idea what I've done

--- lua/soat/groups.lua
+++ lua/soat/groups.lua
@@ -6,13 +6,20 @@ return {
 	Normal 				= { bg = c.bg 			},
 	NormalFloat			= { bg = c.bg_lighter 		},
 	Float 				= { fg = c.bg_lighter 		},
+	StatusLine			= { bg = c.bg_lighter		},
 
-  	--Treesitter
+	-- Keywords, literals
+    	String	 			= { fg = c.string 		},
     	["@string"] 			= { fg = c.string 		},
+    	["@number"] 			= { fg = c.string, 		},
+    	Keyword 			= { fg = c.keyword, bold = true },
     	["@keyword"] 			= { fg = c.keyword, bold = true },
+    	["@lsp.type.keyword"] 		= { fg = c.keyword, bold = true },
 
 	-- Variables
+	Identifier 			= { fg = c.variable 		},
 	["@variable"] 			= { fg = c.variable 		},
+	["@lsp.type.variable"] 		= { fg = c.variable 		},
 	["@variable.builtin"] 		= { fg = c.keyword  		},
 	["@variable.parameter"] 	= { fg = c.variable 		},
 	["@variable.parameter.builtin"] = { fg = c.keyword  		},
@@ -28,19 +35,26 @@ return {
 	["@label"]			= { fg = c.keyword  		},
 
 	-- Functions
+	Function			= { fg = c.Function		},
 	["@function"]			= { fg = c.Function		},
 	["@function.builtin"]		= { fg = c.Function		},
 	["@function.call"] 		= { fg = c.Function		},
 	["@function.macro"] 		= { fg = c.Function		},
 
 	-- Types
-	["@type"] 			= { fg = c.type			},
-	["@type.builtin"]		= { fg = c.type			},
-	["@type.definition"]		= { fg = c.type			},
+	Type	 			= { fg = c.type, bold = true	},
+	["@type"] 			= { fg = c.type, bold = true	},
+	["@type.builtin"]		= { fg = c.type, bold = true	},
+	["@type.definition"]		= { fg = c.type, bold = true	},
 
 	-- Attributes / Properties
 	["@attribute"]			= { fg = c.attribute		},
 	["@attribute.builtin"]		= { fg = c.attribute		},
 	["@property"]			= { fg = c.attribute		},
+
+	-- Punctuation
+	["@punctuation.delimiter"]	= { fg = c.punctuation		},
+	["@punctuation.bracket"]	= { fg = c.punctuation		},
+	["@punctuation.special"]	= { fg = c.punctuation		},
 }
 
--- lua/soat/palette.lua
+++ lua/soat/palette.lua
@@ -2,17 +2,18 @@ return {
 	-- element approach
 	bg 	   	= "#161616",
 	bg_lighter 	= "#262626",
-	bg_lightest 	= "#262626",
+	bg_lightest 	= "#363636",
 	comment    	= "#606060",
 
 	-- Syntax
-	keyword    	= "#e6b800",
+	keyword    	= "#f6c810",
 	variable   	= "#F0F0F0",
-	Function   	= "#abd6f7",
+	punctuation	= "#a86782",
+	Function   	= "#8bb6e7",
 	attribute 	= "#ff99cc",
 	string     	= "#f9e2af",
 	constant   	= "#cdd6f4",
-	type       	= "#cba6f7",
+	type       	= "#f6c810",
 	number     	= "#fab387",
 	boolean    	= "#f38ba8",
 	operator   	= "#94a3b8",
