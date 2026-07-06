38138ef799bf160055372b932cad8ef7edb8a5f5
Marian Guceanu <marian.guceanu@yahoo.com>
2026-07-06 11:53:31 +0300

    all keywords colored and more beautiful colors

--- lua/soat/groups.lua
+++ lua/soat/groups.lua
@@ -1,60 +1,127 @@
 local c = require("soat.palette")
 
-return {
-	-- Standard
-    	Comment 			= { fg = c.comment 		},
-	Normal 				= { bg = c.bg 			},
-	NormalFloat			= { bg = c.bg_lighter 		},
-	Float 				= { fg = c.bg_lighter 		},
-	StatusLine			= { bg = c.bg_lighter		},
+local function add_group_to_table(source, destination)
+	for k, v in pairs(source) do
+		destination[k] = v
+	end
+end
 
-	-- Keywords, literals
-    	String	 			= { fg = c.string 		},
-    	["@string"] 			= { fg = c.string 		},
-    	["@number"] 			= { fg = c.string, 		},
-    	Keyword 			= { fg = c.keyword, bold = true },
-    	["@keyword"] 			= { fg = c.keyword, bold = true },
-    	["@lsp.type.keyword"] 		= { fg = c.keyword, bold = true },
+local s = {}
 
-	-- Variables
-	Identifier 			= { fg = c.variable 		},
-	["@variable"] 			= { fg = c.variable 		},
-	["@lsp.type.variable"] 		= { fg = c.variable 		},
-	["@variable.builtin"] 		= { fg = c.keyword  		},
-	["@variable.parameter"] 	= { fg = c.variable 		},
-	["@variable.parameter.builtin"] = { fg = c.keyword  		},
-
-	-- Constants
-	["@constant"]			= { fg = c.constant 		},
-	["@constant.builtin"]		= { fg = c.constant 		},
-	["@constant.macro"]		= { fg = c.constant 		},
-
-	-- Modules / namespaces
-	["@module"]			= { fg = c.keyword  		},
-	["@module.builtin"]		= { fg = c.keyword  		},
-	["@label"]			= { fg = c.keyword  		},
-
-	-- Functions
-	Function			= { fg = c.Function		},
-	["@function"]			= { fg = c.Function		},
-	["@function.builtin"]		= { fg = c.Function		},
-	["@function.call"] 		= { fg = c.Function		},
-	["@function.macro"] 		= { fg = c.Function		},
-
-	-- Types
-	Type	 			= { fg = c.type, bold = true	},
-	["@type"] 			= { fg = c.type, bold = true	},
-	["@type.builtin"]		= { fg = c.type, bold = true	},
-	["@type.definition"]		= { fg = c.type, bold = true	},
-
-	-- Attributes / Properties
-	["@attribute"]			= { fg = c.attribute		},
-	["@attribute.builtin"]		= { fg = c.attribute		},
-	["@property"]			= { fg = c.attribute		},
-
-	-- Punctuation
-	["@punctuation.delimiter"]	= { fg = c.punctuation		},
-	["@punctuation.bracket"]	= { fg = c.punctuation		},
-	["@punctuation.special"]	= { fg = c.punctuation		},
+local editor_defaults = {
+	Normal 				= { bg = c.bg },
+	NormalFloat			= { bg = c.bg },
+	Float 				= { fg = c.bg_lighter },
+	StatusLine			= { bg = c.bg_lighter, fg = "white" },
 }
 
+local comments = {
+    	Comment 			= { fg = c.comment },
+	["@comment"]			= { fg = c.comment },
+	["@comment.documentation"]	= { fg = c.comment },
+}
+
+
+local literals = {
+    	String	 	= { fg = c.string },
+    	["@string"] 	= { fg = c.string },
+    	["@number"] 	= { fg = c.string },
+}
+
+local keywords = {
+    	Keyword 				= { fg = c.keyword },
+    	["@lsp.type.keyword"] 			= { fg = c.keyword },
+    	["@keyword"] 				= { fg = c.keyword },
+	["@keyword.coroutine"]      		= { fg = c.keyword },
+	["@keyword.function"]			= { fg = c.keyword },
+	["@keyword.operator"]  			= { fg = c.keyword },
+	["@keyword.import"]    			= { fg = c.keyword },
+	["@keyword.type"]      			= { fg = c.keyword },
+	["@keyword.modifier"]  			= { fg = c.keyword },
+	["@keyword.repeat"]    			= { fg = c.keyword },
+	["@keyword.return"]    			= { fg = c.keyword },
+	["@keyword.debug"]     			= { fg = c.keyword },
+	["@keyword.exception"] 			= { fg = c.keyword },
+	["@keyword.conditional"] 		= { fg = c.keyword },
+	["@keyword.conditional.ternary"] 	= { fg = c.keyword },
+	["@keyword.directive"] 			= { fg = c.keyword },
+	["@keyword.directive.define"] 		= { fg = c.keyword },
+}
+
+local variables = {
+	Identifier 			= { fg = c.variable },
+	["@variable"] 			= { fg = c.variable },
+	["@lsp.type.variable"] 		= { fg = c.variable },
+	["@variable.parameter"] 	= { fg = c.variable },
+	["@variable.builtin"] 		= { fg = c.keyword  },
+	["@variable.parameter.builtin"] = { fg = c.keyword  },
+}
+
+local constants = {
+	["@constant"]		= { fg = c.constant },
+	["@constant.builtin"]	= { fg = c.constant },
+	["@constant.macro"]	= { fg = c.constant },
+}
+
+local modules = {
+	-- Modules / namespaces
+	["@module"]		= { fg = c.keyword },
+	["@module.builtin"]	= { fg = c.keyword },
+	["@label"]		= { fg = c.keyword },
+}
+
+local functions = {
+	Function		= { fg = c.Function },
+	["@function"]		= { fg = c.Function },
+	["@function.builtin"]	= { fg = c.Function },
+	["@function.call"] 	= { fg = c.Function },
+	["@function.macro"] 	= { fg = c.Function },
+}
+
+local types = {
+	Type	 		= { fg = c.type },
+	["@type"] 		= { fg = c.type },
+	["@type.builtin"]	= { fg = c.type },
+	["@type.definition"]	= { fg = c.type },
+}
+
+local attributes = {
+	["@attribute"]		= { fg = c.attribute },
+	["@attribute.builtin"]	= { fg = c.attribute },
+	["@property"]		= { fg = c.attribute },
+}
+
+local punctuation = {
+	["@punctuation.delimiter"]	= { fg = c.punctuation },
+	["@punctuation.bracket"]	= { fg = c.punctuation },
+	["@punctuation.special"]	= { fg = c.punctuation },
+}
+
+local tags = {
+	["@tag"] 		= { fg = c.operator },
+	["@tag.builtin"] 	= { fg = c.operator },
+	["@tag.attribute"] 	= { fg = c.operator },
+	["@tag.delimiter"] 	= { fg = c.operator }
+}
+
+local file_browser = {
+	OilDirIcon 	= { fg = c.type },
+	OilDir 		= { fg = c.type }
+}
+
+
+add_group_to_table(editor_defaults, s)
+add_group_to_table(comments, s)
+add_group_to_table(literals, s)
+add_group_to_table(keywords, s)
+add_group_to_table(variables, s)
+add_group_to_table(constants, s)
+add_group_to_table(modules, s)
+add_group_to_table(functions, s)
+add_group_to_table(types, s)
+add_group_to_table(attributes, s)
+add_group_to_table(punctuation, s)
+add_group_to_table(tags, s)
+add_group_to_table(file_browser, s)
+
+return s
--- lua/soat/palette.lua
+++ lua/soat/palette.lua
@@ -1,19 +1,19 @@
 return {
 	-- element approach
-	bg 	   	= "#161616",
-	bg_lighter 	= "#262626",
-	bg_lightest 	= "#363636",
+	bg 	   	= "#202020",
+	bg_lighter 	= "#303030",
+	bg_lightest 	= "#404040",
 	comment    	= "#606060",
 
 	-- Syntax
-	keyword    	= "#f6c810",
-	variable   	= "#F0F0F0",
+	keyword    	= "#d6a80e",
+	variable   	= "#f8f8f8",
 	punctuation	= "#a86782",
 	Function   	= "#8bb6e7",
 	attribute 	= "#ff99cc",
 	string     	= "#f9e2af",
 	constant   	= "#cdd6f4",
-	type       	= "#f6c810",
+	type       	= "#60bf90",
 	number     	= "#fab387",
 	boolean    	= "#f38ba8",
 	operator   	= "#94a3b8",
@@ -21,6 +21,6 @@ return {
 	-- Diagnostics
 	diag_error 	= "#ff0000",
 	diag_warn  	= "#fab387",
-	diag_info  	= "#00ff00",
-	diag_hint  	= "#b4befe",
+	diag_info  	= "#e0e0e0",
+	diag_hint  	= "#a6b7f8",
 }
